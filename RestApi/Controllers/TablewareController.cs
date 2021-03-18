9e0abf3fc30add359f8dd8a87624994d6f3f3d61
iltoeeai <tadas.kuprinskas@gmail.com>
2021-03-18 19:34:04 +0200

    created a generic ItemService class

--- RestApi/Controllers/TablewareController.cs
+++ RestApi/Controllers/TablewareController.cs
@@ -34,7 +34,6 @@ namespace RestApi.Controllers
             {
                 throw new KeyNotFoundException();
             }
-            _dataService.TablewareItems.Remove(tablewareItem);
 
             return tablewareItem;
         }
--- RestApi/Services/ItemService.cs
+++ RestApi/Services/ItemService.cs
@@ -1,11 +1,58 @@
-﻿using System;
+﻿using RestApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RestApi.Services
 {
-    public class ItemService<T>
+    public class ItemService<T> : IItemService<T> where T : Item
     {
+        public List<T> Items { get; set; } = new();
+
+        public void Create(T t)
+        {
+            Items.Add(t);
+        }
+
+        public void DeleteById(int id)
+        {
+            var kitchenItem = Items.FirstOrDefault(i => i.Id == id);
+
+            if (kitchenItem == null)
+            {
+                throw new KeyNotFoundException();
+            }
+            Items.Remove(kitchenItem);
+        }
+
+        public List<T> GetAll()
+        {
+            return Items;
+        }
+
+        public T GetById(int id)
+        {
+            var kitchenItem = Items.FirstOrDefault(i => i.Id == id);
+
+            if (kitchenItem == null)
+            {
+                throw new KeyNotFoundException();
+            }            
+
+            return kitchenItem;
+        }
+
+        public void Update(T t)
+        {
+            var kitchenItemToReplace = Items.FirstOrDefault(i => i.Id == t.Id);
+
+            if (kitchenItemToReplace == null)
+            {
+                throw new KeyNotFoundException();
+            }
+            Items[t.Id] = t;
+        }
     }
+
 }
