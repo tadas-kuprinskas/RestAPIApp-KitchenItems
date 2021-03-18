5e5ebc7deec5bba85fdf44f4b344ddbed8013959
iltoeeai <tadas.kuprinskas@gmail.com>
2021-03-18 23:23:01 +0200

    added swagger API library

--- RestApi/Startup.cs
+++ RestApi/Startup.cs
@@ -32,6 +32,9 @@ namespace RestApi
             services.AddSingleton<IItemService<Fruit>, ItemService<Fruit>>();
             services.AddSingleton<IItemService<Vegetable>, ItemService<Vegetable>>();
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +44,16 @@ namespace RestApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseHttpsRedirection();
 
